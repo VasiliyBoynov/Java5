e0144a72ac5dded951a6f93c534216156092340b
Бойнов Василий Николаевич <vnboynov@sberbank.ru>
2022-10-21 22:52:38 +0300

    Lesson_3. Реализация патерна Builder, Singelton.

--- architekt/http-server/src/main/java/ru/geekbrains/Parser/SimpleParser.java
+++ architekt/http-server/src/main/java/ru/geekbrains/Parser/SimpleParser.java
@@ -23,47 +23,39 @@ public class SimpleParser implements RequestParser
 	public HttpRequest parse(List<String> rawRequest)
 	{
 		Log.info("Старт преобразования запроса");
-		HttpRequest request = new HttpRequest();
+
 		try
 		{
-			request.setMethod(
-				Arrays.stream(Metod.values())
-					.map(Enum::name)
-					.filter(m -> rawRequest.get(0).startsWith(m))
-					.findFirst()
-					.orElseThrow(IllegalArgumentException::new));
+			String method = Arrays.stream(Metod.values())
+				.map(Enum::name)
+				.filter(m -> rawRequest.get(0).startsWith(m))
+				.findFirst()
+				.orElseThrow(IllegalArgumentException::new);
 
-			request.setPath(Arrays.stream(rawRequest.get(0).split(" "))
+			String path = Arrays.stream(rawRequest.get(0).split(" "))
 				.skip(1L)
 				.findFirst()
-				.orElseThrow(IllegalArgumentException::new));
+				.orElseThrow(IllegalArgumentException::new);
 
 			Map<String, String> map = new HashMap<>();
 
 			rawRequest.stream()
 				.filter(str -> str.contains(": "))
 				.forEach(s -> {
-				String[] str = s.split(": ");
-				if (str[0] != null)
-				{
-					map.put(str[0], str[1]);
-				}
-			});
-
-			request.setHeaders(map);
-
-			if (request.getPath().contains("?"))
-			{
-				Map<String, String> param = new HashMap<>();
-				Arrays.stream(request.getPath().split("\\?")).skip(1L).findFirst().ifPresent(str -> {
-					for (String s : str.split("&"))
+					String[] str = s.split(": ");
+					if (str[0] != null)
 					{
-						String[] sArray = s.split("=");
-						param.put(sArray[0], sArray[1]);
+						map.put(str[0], str[1]);
 					}
-					request.setParameters(param);
 				});
-			}
+
+			HttpRequest request = HttpRequest.createRequest()
+				.withMethod(method)
+				.withPath(path)
+				.withHeaders(map)
+				.build();
+
+			return request;
 		}
 		catch (Exception e)
 		{
@@ -71,6 +63,6 @@ public class SimpleParser implements RequestParser
 			Log.info("Не возможно преобразовать в HTTP запрос");
 			return null;
 		}
-		return request;
+
 	}
 }
--- architekt/http-server/src/main/java/ru/geekbrains/domain/HttpResponse.java
+++ architekt/http-server/src/main/java/ru/geekbrains/domain/HttpResponse.java
@@ -5,27 +5,47 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.io.Reader;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter
-@Setter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class HttpResponse
 {
     private int statusCode;
     private String status;
-    @Setter(AccessLevel.PRIVATE)
     private Map<String, String> headers = new HashMap<>();
-
-    public void putHeaders(String key, String value){
-        headers.put(key, value);
-    }
 
     public Map<String, String> getHeaders()
     {
         return Collections.unmodifiableMap(headers);
+    }
+
+    public static Builder createResponse(){
+        return new Builder();
+    }
+
+    public static class Builder{
+        private HttpResponse response;
+
+        private Builder()
+        {
+            this.response = new HttpResponse();
+        }
+
+        public Builder withStatusCode(int statusCode){
+            this.response.statusCode = statusCode;
+            return this;
+        }
+
+        public Builder withStatus(String status){
+            this.response.status = status;
+            return this;
+        }
+
+        public HttpResponse build(){
+            return response;
+        }
     }
 }
